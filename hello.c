ca228aded99a4fe27fdd181d0b2aa9b4ea453204
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-06 09:04:10 +0700

    First commit: Hello World in C

---
+++ hello.c
@@ -0,0 +1,10 @@
+// Chương trình đầu tiên 6/12/2025 8:33 AM
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello, My name is Khoa!\n");
+    printf("Let's start the journey.");
+    getchar();
+    return 0;
+}
