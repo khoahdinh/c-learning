ff6c4b7e09feeab836023fee2be2c6dfa731f880
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-06 15:39:03 +0700

    Insert comments

--- 01-basic-syntax/hello.c
+++ 01-basic-syntax/hello.c
@@ -1,6 +1,8 @@
 // Chương trình đầu tiên 6/12/2025 8:33 AM
 
-#include <stdio.h>
+/*header file library that lets us 
+work with input and output functions */
+#include <stdio.h> 
 
 int main(){
     printf("Hello, My name is Khoa!\n");
---
+++ 01-basic-syntax/variables.c
@@ -0,0 +1,8 @@
+// Variables in C
+#include <stdio.h>
+
+int main(){
+    int myAge = 15;
+    char myName = 'K';
+    printf("My age is %d and my name is %c\n", myAge, myName);
+}
