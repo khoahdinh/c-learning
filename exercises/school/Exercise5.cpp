4eb88d3ab19215bfe8816c74c9d86c95a6e5622e
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-11 19:01:05 +0700

    Add Exercise5.cpp

---
+++ exercises/school/Exercise5.cpp
@@ -0,0 +1,28 @@
+// Format file: clang-format -i yourfile.c
+/*
+Bài 5: Viết chương trình nhập vào một số nguyên dương 𝑁. Tính và in ra tổng 𝑆 = 1 + 2 + 3 +
+⋯ + 𝑁.
+*/
+
+#include <stdio.h>
+
+int main() {
+  // Input: n
+  int n;
+
+  printf("Nhap so n: ");
+  scanf("%d", &n);
+
+  // Check n > 0
+  if (n<0)
+  {
+    printf("Nhap sai n");
+    return 0;
+  }
+  
+  int sum = (n*(n+1))/2;
+
+  printf("Tong tu 1 - %d = %d",n,sum);
+
+  return 0;
+}
