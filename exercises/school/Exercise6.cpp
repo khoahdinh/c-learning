97f6e973f084b7c1a8e63a9f6f9ea70502d57040
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-11 19:18:57 +0700

    Add Exercise6.cpp

---
+++ exercises/school/Exercise6.cpp
@@ -0,0 +1,36 @@
+// Format file: clang-format -i yourfile.c
+/*
+Bài 6: Viết chương trình nhập vào một số nguyên không âm 𝑁! (𝑁 > 0). Tính và in
+ra 𝑁! • Quy ước: 0! = 1.
+*/
+
+#include <stdio.h>
+
+int main() {
+  // Input: n
+  int n, result = 1;
+
+  printf("Nhap so n: ");
+  scanf("%d", &n);
+
+  // Check n >= 0
+  if (n < 0) {
+    printf("Nhap sai n");
+    return 0;
+  }
+
+  // Solve...
+  if (n == 0) {
+    result = 1;
+  } else {
+    int i = 1;
+    while (i <= n) {
+      result = result * i;
+      i++;
+    }
+  }
+
+  printf("Ket qua giai thua cua so %d! = %d", n, result);
+
+  return 0;
+}
