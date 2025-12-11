957160d732f527ec1d0237058c12577859848179
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-11 12:34:27 +0700

    Add file Exercise1.cpp

---
+++ exercises/school/Exercise1.cpp
@@ -0,0 +1,32 @@
+/*
+Bài 1: Viết chương trình nhập vào chiều dài và chiều rộng của một hình chữ nhật
+(kiểu số thực). Tính toán và in ra màn hình chu vi và diện tích của hình chữ
+nhật đó.
+*/
+
+// clang-format -i yourfile.c -> Format file
+
+#include <stdio.h>
+
+int main() {
+  // Input: rectangle length and width
+  float length, width;
+
+  printf("Nhap chieu dai: ");
+  scanf("%f", &length);
+
+  printf("Nhap chieu rong: ");
+  scanf("%f", &width);
+
+  // Calculate perimeter and area
+  float perimeter = (length + width) * 2;
+  float area = length * width;
+
+  // Output result
+  printf("Chu vi cua hinh chu nhat la: %.2f \n", perimeter);
+  printf("Dien tich cua hinh chu nhat la: %.2f", area);
+
+  return 0;
+}
+
+
