184f2e4b6686ef79bcbf881ca46929cf197114ef
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-14 16:18:42 +0700

    Add Exercise27.cpp

---
+++ exercises/school/Exercise27.cpp
@@ -0,0 +1,41 @@
+// Format file: clang-format -i yourfile.c
+/*
+Bài 27: Viết chương trình nhập vào một số nguyên dương 𝑁 (𝑁 > 2). In ra 𝑁 số đầu
+tiên trong dãy Fibonacci. Dãy Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13
+*/
+
+#include <stdio.h>
+
+int main() {
+  // Input: n,
+  int n;
+
+  printf("Nhap so N nguyen duong (N>2): ");
+  scanf("%d", &n);
+
+  // Check N
+  if (n <= 2) {
+    printf("N phai lon hon 2!");
+    return 0;
+  }
+
+  // First two Fibonacci numbers
+  int first = 0;
+  int second = 1;
+  int next;
+
+  // Print first two numbers
+  printf("%d %d", first, second);
+
+  /// Generate remaining Fibonacci numbers
+  for (int i = 3; i <= n; i++) {
+    next = first + second;
+    printf(" %d", next);
+
+    // Update values for next iteration
+    first = second;
+    second = next;
+  }
+
+  return 0;
+}
