c1b92205666bac281a69e921a0dc4ca963b7d660
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-25 13:37:42 +0700

    Add check.h

---
+++ exercises/school/functions/check.h
@@ -0,0 +1,16 @@
+#ifndef CHECK_H
+#define CHECK_H
+
+bool isEven(int n);
+bool isOdd(int n);
+bool isPositive(int n);
+bool isPrime(int n);
+bool isSquareNumber(int n);
+bool isPerfectNumber(int n);
+bool isLeapYear(int year); 
+bool isDivisible(int number, int divisor); 
+bool isValidTriangle(float a, float b, float c); 
+bool isVowel(char c);
+bool isDigit(char c);
+
+#endif
