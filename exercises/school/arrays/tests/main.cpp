a273e731f26dd5b91dc01d841e609dbb241768bb
khoa.hdinh <khoa.hdinh@gmail.com>
2025-12-30 10:26:12 +0700

    Update main.cpp

--- exercises/school/arrays/tests/main.cpp
+++ exercises/school/arrays/tests/main.cpp
@@ -29,47 +29,46 @@ int main() {
     return 0;
   }
 
-//   Nhóm 1: Nhập, Xuất và Tính toán cơ bản
+  //   Nhóm 1: Nhập, Xuất và Tính toán cơ bản
 
-//   phatSinhMangNgauNhien(a, n);
+  //   phatSinhMangNgauNhien(a, n);
   nhapMang(a, n);
   xuatMang(a, n);
 
-//   int tongMang = tinhTongMang(a, n);
-//   printf("Tong = %d\n", tongMang);
+  //   int tongMang = tinhTongMang(a, n);
+  //   printf("Tong = %d\n", tongMang);
 
-//   long long tichMang = tinhTichMang(a, n);
-//   printf("Tich = %d\n", tichMang);
+  //   long long tichMang = tinhTichMang(a, n);
+  //   printf("Tich = %d\n", tichMang);
 
-//   float trungBinhCong = tinhTrungBinhCong(a, n);
-//   printf("Trung binh cong = %.2f\n", trungBinhCong);
+  //   float trungBinhCong = tinhTrungBinhCong(a, n);
+  //   printf("Trung binh cong = %.2f\n", trungBinhCong);
 
-//   int tongChan = tinhTongPhanTuChan(a, n);
-//   printf("Tong chan = %d\n", tongChan);
+  //   int tongChan = tinhTongPhanTuChan(a, n);
+  //   printf("Tong chan = %d\n", tongChan);
 
-//   int tongLeViTriLe = tinhTongPhanTuLeViTriLe(a, n);
-//   printf("Tong le cua phan tu vi tri le = %d\n", tongLeViTriLe);
+  //   int tongLeViTriLe = tinhTongPhanTuLeViTriLe(a, n);
+  //   printf("Tong le cua phan tu vi tri le = %d\n", tongLeViTriLe);
 
-// Nhóm 2: Tìm kiếm Giá trị và Vị trí
+  // Nhóm 2: Tìm kiếm Giá trị và Vị trí
 
-    // int phanTuAmLonNhat = timPhanTuAmLonNhat(a,n);
-    // printf("Phan tu am lon nhat = %d\n", phanTuAmLonNhat);
+  // int phanTuAmLonNhat = timPhanTuAmLonNhat(a,n);
+  // printf("Phan tu am lon nhat = %d\n", phanTuAmLonNhat);
 
-    // int phanTuDuongNhoNhat = timPhanTuDuongNhoNhat(a,n);
-    // printf("Phan tu duong nho nhat = %d\n", phanTuDuongNhoNhat);
+  // int phanTuDuongNhoNhat = timPhanTuDuongNhoNhat(a,n);
+  // printf("Phan tu duong nho nhat = %d\n", phanTuDuongNhoNhat);
 
-    // int viTriXDauTien = timViTriDauTien(a,n,5);
-    // printf("Vi tri dau tien cua X trong mang: = %d\n", viTriXDauTien);
+  // int viTriXDauTien = timViTriDauTien(a,n,5);
+  // printf("Vi tri dau tien cua X trong mang: = %d\n", viTriXDauTien);
 
-    // int viTriXCuoiCung = timViTriCuoiCung(a,n,10);
-    // printf("Vi tri cuoi cung cua X trong mang: = %d\n", viTriXCuoiCung);
+  // int viTriXCuoiCung = timViTriCuoiCung(a,n,10);
+  // printf("Vi tri cuoi cung cua X trong mang: = %d\n", viTriXCuoiCung);
 
-    // int soChanDauTien = timSoChanDauTien(a,n);
-    // printf("So chan dau tien trong mang: = %d\n", soChanDauTien);
+  // int soChanDauTien = timSoChanDauTien(a,n);
+  // printf("So chan dau tien trong mang: = %d\n", soChanDauTien);
 
-    // int soLeCuoiCung = timSoLeCuoiCung(a,n);
-    // printf("So le cuoi cung trong mang: = %d\n", soLeCuoiCung);
-
+  // int soLeCuoiCung = timSoLeCuoiCung(a,n);
+  // printf("So le cuoi cung trong mang: = %d\n", soLeCuoiCung);
 
   return 0;
 }
