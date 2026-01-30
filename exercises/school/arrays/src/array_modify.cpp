40ca268ca0efae408efe0fcb89f0d5d6d7e4f5b9
khoa.hdinh <khoa.hdinh@gmail.com>
2026-01-30 14:44:31 +0700

    Add array_modify.cpp

--- exercises/school/arrays/src/array_modify.cpp
+++ exercises/school/arrays/src/array_modify.cpp
@@ -40,39 +40,31 @@ void themPhanTuViTriK(int a[], int &n, int x, int k) {
 }
 
 // 28. Viết hàm xóa phần tử cuối mảng.
-void xoaPhanTuCuoi(int a[], int &n){
-
-  n--;
-}
-
+void xoaPhanTuCuoi(int a[], int &n) { n--; }
 
 // 29. Viết hàm xóa phần tử đầu mảng.
-void xoaPhanTuDau(int a[], int &n){
+void xoaPhanTuDau(int a[], int &n) {
 
-
-  for (int  i = 0; i < n - 1; i++)
-  {
+  for (int i = 0; i < n - 1; i++) {
     a[i] = a[i + 1];
   }
 
   n--;
-  
 }
 
 // 30. Viết hàm xóa phần tử tại vị trí 𝑘.
-void xoaPhanTuViTriK(int a[], int &n, int k){
+void xoaPhanTuViTriK(int a[], int &n, int k) {
 
-    if (k < 1 || k > n) {
+  if (k < 1 || k > n) {
     printf("Vi tri xoa phan tu nam ngoai pham vi mang!\n");
     return;
   }
 
   // Dịch chuyển các phần tử từ vị trí k về sau sang trái
-  for (int i = k - 1; i < n - 1; i++) { // i = k - 1 để đúng với góc nhìn user 
+  for (int i = k - 1; i < n - 1; i++) { // i = k - 1 để đúng với góc nhìn user
     a[i] = a[i + 1];
   }
   n--;
-
 }
 
 // 31.Viết hàm xóa tất cả các phần tử có giá trị bằng 𝑥 trong mảng.
@@ -98,22 +90,19 @@ void xoaPhanTuViTriK(int a[], int &n, int k){
 //   }
 
 //   n = soLuongPhanTu;
-  
+
 // }
 
-void xoaTatCaPhanTuX(int a[], int &n, int x){
+void xoaTatCaPhanTuX(int a[], int &n, int x) {
 
   int viTriGhi = 0;
 
-  for (int i = 0; i < n; i++)
-  {
-    if (a[i] != x)
-    {
+  for (int i = 0; i < n; i++) {
+    if (a[i] != x) {
       a[viTriGhi] = a[i];
       viTriGhi++;
     }
   }
 
   n = viTriGhi;
-
 }
